e09e599befff53b1b639e1590ec44c21b58d685b
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-15 17:41:51 +0100

    Identify the aggregate in cancelnotificationreceivedcommand

--- payment-service/src/main/java/com/hedvig/paymentservice/domain/trustlyOrder/commands/CancelNotificationReceivedCommand.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/trustlyOrder/commands/CancelNotificationReceivedCommand.java
@@ -2,11 +2,15 @@ package com.hedvig.paymentservice.domain.trustlyOrder.commands;
 
 import java.util.UUID;
 
+import org.axonframework.commandhandling.TargetAggregateIdentifier;
+
 import lombok.Value;
 
 @Value
 public class CancelNotificationReceivedCommand {
+    @TargetAggregateIdentifier
     UUID hedvigOrderId;
+
     String notificationId;
     String trustlyOrderId;
     String memberId;
