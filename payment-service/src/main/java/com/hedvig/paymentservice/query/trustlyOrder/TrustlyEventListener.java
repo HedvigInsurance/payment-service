c3e001e0e8d0e47691f3840b37aebc18b5d09dc8
meletios <meletios@hedvig.com>
2018-09-25 14:00:28 +0200

    upgrade spring boot to 2.0.4, fix issues related to the upgrade

--- payment-service/src/main/java/com/hedvig/paymentservice/query/trustlyOrder/TrustlyEventListener.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/trustlyOrder/TrustlyEventListener.java
@@ -17,71 +17,71 @@ import org.springframework.stereotype.Component;
 @Component
 public class TrustlyEventListener {
 
-  private final TrustlyOrderRepository orderRepository;
+    private final TrustlyOrderRepository orderRepository;
 
-  public TrustlyEventListener(TrustlyOrderRepository orderRepository) {
-    this.orderRepository = orderRepository;
-  }
+    public TrustlyEventListener(TrustlyOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
-  @EventHandler
-  public void on(OrderCreatedEvent e) {
-    TrustlyOrder order = new TrustlyOrder();
+    @EventHandler
+    public void on(OrderCreatedEvent e) {
+        TrustlyOrder order = new TrustlyOrder();
 
-    order.setId(e.getHedvigOrderId());
-    order.setMemberId(e.getMemberId());
+        order.setId(e.getHedvigOrderId());
+        order.setMemberId(e.getMemberId());
 
-    orderRepository.save(order);
-  }
+        orderRepository.save(order);
+    }
 
-  @EventHandler
-  public void on(OrderAssignedTrustlyIdEvent e) {
-    TrustlyOrder order = orderRepository.findOne(e.getHedvigOrderId());
-    order.setState(OrderState.CONFIRMED);
-    order.setTrustlyOrderId(e.getTrustlyOrderId());
-    orderRepository.save(order);
-  }
+    @EventHandler
+    public void on(OrderAssignedTrustlyIdEvent e) {
+        TrustlyOrder order = orderRepository.findById(e.getHedvigOrderId()).get();
+        order.setState(OrderState.CONFIRMED);
+        order.setTrustlyOrderId(e.getTrustlyOrderId());
+        orderRepository.save(order);
+    }
 
-  @EventHandler
-  public void on(OrderCanceledEvent e) {
-    TrustlyOrder order = orderRepository.findOne(e.getHedvigOrderId());
-    order.setState(OrderState.CANCELED);
-    orderRepository.save(order);
-  }
+    @EventHandler
+    public void on(OrderCanceledEvent e) {
+        TrustlyOrder order = orderRepository.findById(e.getHedvigOrderId()).get();
+        order.setState(OrderState.CANCELED);
+        orderRepository.save(order);
+    }
 
-  @EventHandler
-  public void on(OrderCompletedEvent e) {
-    TrustlyOrder order = orderRepository.findOne(e.getId());
-    order.setState(OrderState.COMPLETE);
-    orderRepository.save(order);
-  }
+    @EventHandler
+    public void on(OrderCompletedEvent e) {
+        TrustlyOrder order = orderRepository.findById(e.getId()).get();
+        order.setState(OrderState.COMPLETE);
+        orderRepository.save(order);
+    }
 
-  @EventHandler
-  public void on(SelectAccountResponseReceivedEvent e) {
-    TrustlyOrder order = orderRepository.findOne(e.getHedvigOrderId());
-    order.setIframeUrl(e.getIframeUrl());
-    order.setType(OrderType.SELECT_ACCOUNT);
-    orderRepository.save(order);
-  }
+    @EventHandler
+    public void on(SelectAccountResponseReceivedEvent e) {
+        TrustlyOrder order = orderRepository.findById(e.getHedvigOrderId()).get();
+        order.setIframeUrl(e.getIframeUrl());
+        order.setType(OrderType.SELECT_ACCOUNT);
+        orderRepository.save(order);
+    }
 
-  @EventHandler
-  public void on(AccountNotificationReceivedEvent e) {
-    TrustlyOrder order = orderRepository.findByTrustlyOrderId(e.getTrustlyOrderId());
-    TrustlyNotification notification = new TrustlyNotification();
-    notification.setNotificationId(e.getNotificationId());
-    order.addNotification(notification);
+    @EventHandler
+    public void on(AccountNotificationReceivedEvent e) {
+        TrustlyOrder order = orderRepository.findByTrustlyOrderId(e.getTrustlyOrderId());
+        TrustlyNotification notification = new TrustlyNotification();
+        notification.setNotificationId(e.getNotificationId());
+        order.addNotification(notification);
 
-    notification.setAccountId(e.getAccountId());
-    notification.setAddress(e.getAddress());
-    notification.setBank(e.getBank());
-    notification.setCity(e.getCity());
-    notification.setClearingHouse(e.getClearingHouse());
-    notification.setDescriptor(e.getDescriptor());
-    notification.setDirectDebitMandate(e.getDirectDebitMandate());
-    notification.setLastDigits(e.getLastDigits());
-    notification.setName(e.getName());
-    notification.setPersonId(e.getPersonId());
-    notification.setZipCode(e.getZipCode());
+        notification.setAccountId(e.getAccountId());
+        notification.setAddress(e.getAddress());
+        notification.setBank(e.getBank());
+        notification.setCity(e.getCity());
+        notification.setClearingHouse(e.getClearingHouse());
+        notification.setDescriptor(e.getDescriptor());
+        notification.setDirectDebitMandate(e.getDirectDebitMandate());
+        notification.setLastDigits(e.getLastDigits());
+        notification.setName(e.getName());
+        notification.setPersonId(e.getPersonId());
+        notification.setZipCode(e.getZipCode());
 
-    // orderRepository.save(order);
-  }
+        // orderRepository.save(order);
+    }
 }
