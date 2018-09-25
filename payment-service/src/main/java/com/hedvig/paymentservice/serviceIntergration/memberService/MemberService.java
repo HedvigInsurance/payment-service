cd8f2f3d363e911568844ca55bafaeb71810b69f
meletios <meletios@hedvig.com>
2018-09-25 14:37:53 +0200

    add memberService Integration to the payment service

---
+++ payment-service/src/main/java/com/hedvig/paymentservice/serviceIntergration/memberService/MemberService.java
@@ -0,0 +1,10 @@
+package com.hedvig.paymentservice.serviceIntergration.memberService;
+
+import com.hedvig.paymentservice.serviceIntergration.memberService.dto.Member;
+import com.hedvig.paymentservice.serviceIntergration.memberService.dto.SanctionStatus;
+import java.util.Optional;
+
+public interface MemberService {
+  Optional<Member> getMember(String memberId);
+  SanctionStatus getMemberSanctionStatus(String memberId);
+}
