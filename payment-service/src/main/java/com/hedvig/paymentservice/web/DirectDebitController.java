e8f137fe127c4e6ffe1d6d0db45638b66aa0daf5
meletios <meletios@hedvig.com>
2018-10-25 14:25:32 +0200

    alter the DD endpoints, add tests, add new DTO

--- payment-service/src/main/java/com/hedvig/paymentservice/web/DirectDebitController.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/web/DirectDebitController.java
@@ -2,11 +2,20 @@ package com.hedvig.paymentservice.web;
 
 import com.hedvig.paymentservice.query.member.entities.Member;
 import com.hedvig.paymentservice.query.member.entities.MemberRepository;
+import com.hedvig.paymentservice.services.trustly.TrustlyService;
+import com.hedvig.paymentservice.services.trustly.dto.DirectDebitOrderInfo;
+import com.hedvig.paymentservice.web.dtos.DirectDebitResponse;
 import com.hedvig.paymentservice.web.dtos.DirectDebitStatusDTO;
+import com.hedvig.paymentservice.web.dtos.RegisterDirectDebitRequestDTO;
 import java.util.Optional;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +25,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/directDebit/")
 public class DirectDebitController {
 
-  private MemberRepository memberRepository;
+  private Logger logger = LoggerFactory.getLogger(DirectDebitController.class);
 
-  public DirectDebitController(MemberRepository memberRepository) {
+  private MemberRepository memberRepository;
+  private TrustlyService trustlyService;
+
+  public DirectDebitController(MemberRepository memberRepository, TrustlyService trustlyService) {
     this.memberRepository = memberRepository;
+    this.trustlyService = trustlyService;
   }
 
   @GetMapping(path = "status")
   public ResponseEntity<DirectDebitStatusDTO> getDirectDebitStatus(
       @RequestHeader(name = "hedvig.token") String memberId) {
+
+    logger.debug("Fetching status for member {}", memberId);
 
     Optional<Member> om = memberRepository.findById(memberId);
 
@@ -37,4 +52,16 @@ public class DirectDebitController {
     return ResponseEntity
         .ok(new DirectDebitStatusDTO(member.getId(), member.getDirectDebitMandateActive()));
   }
+
+  @PostMapping(path = "register")
+  public ResponseEntity<DirectDebitResponse> registerDirectDebit(
+      @RequestHeader(name = "hedvig.token") String memberId,
+      @RequestBody @Valid RegisterDirectDebitRequestDTO req) {
+
+    final DirectDebitResponse response = trustlyService
+        .requestDirectDebitAccount(new DirectDebitOrderInfo(memberId, req, false));
+
+    return ResponseEntity.ok(response);
+  }
+
 }
