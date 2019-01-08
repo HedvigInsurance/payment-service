5b2bbf8c9a1aab01263080d370a0093af3185314
meletios <meletios@hedvig.com>
2019-01-08 16:03:31 +0100

    make use of the annotation

--- payment-service/src/main/java/com/hedvig/paymentservice/web/TrustlyNotificationController.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/web/TrustlyNotificationController.java
@@ -6,6 +6,7 @@ import com.hedvig.paymentService.trustly.commons.ResponseStatus;
 import com.hedvig.paymentService.trustly.data.notification.Notification;
 import com.hedvig.paymentService.trustly.data.response.Response;
 import com.hedvig.paymentservice.services.trustly.TrustlyService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/hooks/trustly/")
 public class TrustlyNotificationController {
 
-  private final Logger log = LoggerFactory.getLogger(TrustlyNotificationController.class);
   private final TrustlyService trustlyService;
   private final NotificationHandler notificationHandler;
 
