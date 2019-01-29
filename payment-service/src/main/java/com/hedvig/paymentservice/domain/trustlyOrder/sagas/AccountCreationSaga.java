00d0c357e68939d7902b26a4c31e9d2ff4f3a7a1
meletios <meletios@hedvig.com>
2019-01-29 17:20:54 +0100

    move CreateAccountNotificationCommand to commands

--- payment-service/src/main/java/com/hedvig/paymentservice/domain/trustlyOrder/sagas/AccountCreationSaga.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/trustlyOrder/sagas/AccountCreationSaga.java
@@ -1,52 +1,54 @@
-package com.hedvig.paymentservice.domain.trustlyOrder.sagas;
+package com.hedvig.paymentservice.domain.trustlyOrder.sagas
 
-import com.hedvig.paymentservice.domain.payments.commands.CreateMemberCommand;
-import com.hedvig.paymentservice.domain.payments.commands.UpdateTrustlyAccountCommand;
-import com.hedvig.paymentservice.domain.trustlyOrder.events.AccountNotificationReceivedEvent;
-import lombok.val;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.commandhandling.model.AggregateNotFoundException;
-import org.axonframework.eventhandling.saga.EndSaga;
-import org.axonframework.eventhandling.saga.SagaEventHandler;
-import org.axonframework.eventhandling.saga.StartSaga;
-import org.axonframework.spring.stereotype.Saga;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hedvig.paymentservice.domain.payments.commands.CreateMemberCommand
+import com.hedvig.paymentservice.domain.payments.commands.UpdateTrustlyAccountCommand
+import com.hedvig.paymentservice.domain.trustlyOrder.events.AccountNotificationReceivedEvent
+import lombok.`val`
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.commandhandling.model.AggregateNotFoundException
+import org.axonframework.eventhandling.saga.EndSaga
+import org.axonframework.eventhandling.saga.SagaEventHandler
+import org.axonframework.eventhandling.saga.StartSaga
+import org.axonframework.spring.stereotype.Saga
+import org.springframework.beans.factory.annotation.Autowired
 
 @Saga
-public class AccountCreationSaga {
+class AccountCreationSaga {
 
-  @Autowired
-  transient CommandGateway commandGateway;
+    @Autowired
+    @Transient
+    internal var commandGateway: CommandGateway? = null
 
-  @StartSaga
-  @SagaEventHandler(associationProperty = "accountId")
-  @EndSaga
-  public void on(AccountNotificationReceivedEvent event) {
-    try {
-      updateTrustlyAccount(event);
-    } catch (AggregateNotFoundException e) {
-      commandGateway.sendAndWait(new CreateMemberCommand(event.getMemberId()));
-      updateTrustlyAccount(event);
+    @StartSaga
+    @SagaEventHandler(associationProperty = "accountId")
+    @EndSaga
+    fun on(event: AccountNotificationReceivedEvent) {
+        try {
+            updateTrustlyAccount(event)
+        } catch (e: AggregateNotFoundException) {
+            commandGateway!!.sendAndWait<Any>(CreateMemberCommand(event.memberId))
+            updateTrustlyAccount(event)
+        }
+
     }
-  }
 
-  private void updateTrustlyAccount(AccountNotificationReceivedEvent event) {
-    val command =
-      new UpdateTrustlyAccountCommand(
-        event.getMemberId(),
-        event.getHedvigOrderId(),
-        event.getAccountId(),
-        event.getAddress(),
-        event.getBank(),
-        event.getCity(),
-        event.getClearingHouse(),
-        event.getDescriptor(),
-        event.getDirectDebitMandate(),
-        event.getLastDigits(),
-        event.getName(),
-        event.getPersonId(),
-        event.getZipCode());
+    private fun updateTrustlyAccount(event: AccountNotificationReceivedEvent) {
+        val command = UpdateTrustlyAccountCommand(
+            event.memberId,
+            event.hedvigOrderId,
+            event.accountId,
+            event.address,
+            event.bank,
+            event.city,
+            event.clearingHouse,
+            event.descriptor,
+            event.directDebitMandate,
+            event.lastDigits,
+            event.name,
+            event.personId,
+            event.zipCode
+        )
 
-    commandGateway.sendAndWait(command);
-  }
+        commandGateway!!.sendAndWait<Any>(command)
+    }
 }
