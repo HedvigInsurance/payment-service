b0b172d4b9ae08a563efe8034e43cb1cc7d9ff27
meletios <meletios@hedvig.com>
2019-01-30 16:58:19 +0100

    add new grapql endpoint for the status of connecting the bank account

--- payment-service/src/main/java/com/hedvig/paymentservice/graphQl/Query.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/graphQl/Query.java
@@ -2,8 +2,11 @@ package com.hedvig.paymentservice.graphQl;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.hedvig.paymentservice.graphQl.types.BankAccount;
+import com.hedvig.paymentservice.graphQl.types.RegisterAccountProcessingStatus;
 import com.hedvig.paymentservice.query.member.entities.Member;
 import com.hedvig.paymentservice.query.member.entities.MemberRepository;
+import com.hedvig.paymentservice.query.registerAccount.enteties.RegisterAccount;
+import com.hedvig.paymentservice.query.registerAccount.enteties.RegisterAccountRepository;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.servlet.GraphQLContext;
 import javassist.tools.web.BadHttpRequest;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -20,6 +24,7 @@ public class Query implements GraphQLQueryResolver {
 
   private static String HEDVIG_TOKEN = "hedvig.token";
   private MemberRepository memberRepository;
+  private RegisterAccountRepository registerAccountRepository;
 
   public Query(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
@@ -38,6 +43,13 @@ public class Query implements GraphQLQueryResolver {
   //TODO: Catch Red days - Weekends
   public LocalDate getChargeDate() {
     return LocalDate.of(YearMonth.now().getYear(), YearMonth.now().getMonth(), 27);
+  }
+
+  public RegisterAccountProcessingStatus getRegisterAccountProcessingStatus(String orderId) {
+    Optional<RegisterAccount> optionalRegisterAccount = registerAccountRepository.findById(UUID.fromString(orderId));
+    return optionalRegisterAccount.
+      map(registerAccount -> RegisterAccountProcessingStatus.valueOf(registerAccount.getStatus().name()))
+      .orElse(null);
   }
 
   private String getToken(DataFetchingEnvironment dfe) {
