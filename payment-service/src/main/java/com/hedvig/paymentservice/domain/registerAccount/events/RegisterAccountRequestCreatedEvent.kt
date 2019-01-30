f0d4e20c74e22ec9fcdcc0a3fae3d318df684a5d
meletios <meletios@hedvig.com>
2019-01-30 15:48:28 +0100

    refactor events

--- payment-service/src/main/java/com/hedvig/paymentservice/domain/registerAccount/events/RegisterAccountRequestCreatedEvent.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/registerAccount/events/RegisterAccountRequestCreatedEvent.kt
@@ -3,6 +3,6 @@ package com.hedvig.paymentservice.domain.registerAccount.events
 import java.util.*
 
 data class RegisterAccountRequestCreatedEvent(
-  val memberId: String,
-  val hedvigOrderId: UUID
+  val hedvigOrderId: UUID,
+  val memberId: String
 )
--- payment-service/src/main/java/com/hedvig/paymentservice/domain/registerAccount/events/RegisterAccountResponseReceivedEvent.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/registerAccount/events/RegisterAccountResponseReceivedEvent.kt
@@ -3,6 +3,5 @@ package com.hedvig.paymentservice.domain.registerAccount.events
 import java.util.*
 
 data class RegisterAccountResponseReceivedEvent(
-  val hedvigOrderId: UUID,
-  val trustlyUrl: String
+  val hedvigOrderId: UUID
 )
