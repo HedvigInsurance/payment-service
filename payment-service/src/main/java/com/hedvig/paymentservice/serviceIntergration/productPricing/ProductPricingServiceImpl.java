4f3e9626b355b390c967fd50d6284d2ac6095676
meletios <meletios@hedvig.com>
2019-03-11 10:57:39 +0100

    catch 500

--- payment-service/src/main/java/com/hedvig/paymentservice/serviceIntergration/productPricing/ProductPricingServiceImpl.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/serviceIntergration/productPricing/ProductPricingServiceImpl.java
@@ -2,12 +2,14 @@ package com.hedvig.paymentservice.serviceIntergration.productPricing;
 
 import com.hedvig.paymentservice.serviceIntergration.productPricing.dto.InsuranceStatus;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class ProductPricingServiceImpl implements ProductPricingService {
   private ProductPricingClient productPricingClient;
@@ -23,8 +25,11 @@ public class ProductPricingServiceImpl implements ProductPricingService {
       ResponseEntity<InsuranceStatus> response = productPricingClient.getInsuranceStatus(memberId);
       return response.getStatusCode().is2xxSuccessful() ? Optional.ofNullable(response.getBody()) : Optional.empty();
     } catch (FeignException ex) {
-      if (ex.status() == 404) {
-        return Optional.empty();
+      switch (ex.status()){
+        case 500:
+          log.error("Product-pricing returned 500 response");
+        case 404:
+          return Optional.empty();
       }
     }
     return Optional.empty();
