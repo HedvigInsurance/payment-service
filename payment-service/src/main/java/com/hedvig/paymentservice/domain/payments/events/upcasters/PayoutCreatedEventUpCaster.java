f4636aaf7b51c88d474d8c41edc4700b0b949e89
vonElfvin <elvin.granat@gmail.com>
2019-04-08 10:52:24 +0200

    Remove unnecessary comments

--- payment-service/src/main/java/com/hedvig/paymentservice/domain/payments/events/upcasters/PayoutCreatedEventUpCaster.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/payments/events/upcasters/PayoutCreatedEventUpCaster.java
@@ -8,12 +8,10 @@ import org.axonframework.serialization.upcasting.event.SingleEventUpcaster;
 
 public class PayoutCreatedEventUpCaster extends SingleEventUpcaster {
 
-  // Target type of event, class PayoutCreatedEvent with revisionNumber null
   private static SimpleSerializedType targetType = new SimpleSerializedType(
     PayoutCreatedEvent.class.getTypeName(), null
   );
 
-  // Can the event upcast? Its revision is null.
   @Override
   protected boolean canUpcast(IntermediateEventRepresentation intermediateEventRepresentation) {
     return intermediateEventRepresentation.getType().equals(targetType);
