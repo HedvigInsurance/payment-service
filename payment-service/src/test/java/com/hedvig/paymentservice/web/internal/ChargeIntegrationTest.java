50f410f512b707593bdbad979cba97b70a274250
vonElfvin <elvin.granat@gmail.com>
2019-05-24 13:49:50 +0200

    Add v2 Charge endpoint test

--- payment-service/src/test/java/com/hedvig/paymentservice/web/internal/ChargeIntegrationTest.java
+++ payment-service/src/test/java/com/hedvig/paymentservice/web/internal/ChargeIntegrationTest.java
@@ -1,28 +1,5 @@
 package com.hedvig.paymentservice.web.internal;
 
-import static com.hedvig.paymentservice.domain.DomainTestUtilities.hasEvent;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.HEDVIG_ORDER_ID;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_CITY;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_LAST_NAME;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_MEMBER_ID;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_SSN;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_STREET;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVANSSON_ZIP;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TOLVAN_FIRST_NAME;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRANSACTION_AMOUNT;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_BANK;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_CLEARING_HOUSE;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_DESCRIPTOR;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_DIRECTDEBIT_TRUE;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_ID;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ACCOUNT_LAST_DIGITS;
-import static com.hedvig.paymentservice.trustly.testHelpers.TestData.TRUSTLY_ORDER_ID;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hedvig.paymentService.trustly.SignedAPI;
 import com.hedvig.paymentService.trustly.data.response.Error;
@@ -37,8 +14,6 @@ import com.hedvig.paymentservice.domain.payments.events.ChargeCreationFailedEven
 import com.hedvig.paymentservice.domain.trustlyOrder.events.PaymentErrorReceivedEvent;
 import com.hedvig.paymentservice.domain.trustlyOrder.events.PaymentResponseReceivedEvent;
 import com.hedvig.paymentservice.web.dtos.ChargeRequest;
-import java.util.HashMap;
-import java.util.stream.Collectors;
 import lombok.val;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventsourcing.eventstore.EventStore;
@@ -54,6 +29,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import static com.hedvig.paymentservice.domain.DomainTestUtilities.hasEvent;
+import static com.hedvig.paymentservice.trustly.testHelpers.TestData.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -179,6 +166,48 @@ public class ChargeIntegrationTest {
     val trustlyOrderEvents =
         eventStore.readEvents(HEDVIG_ORDER_ID.toString()).asStream().collect(Collectors.toList());
     assertThat(trustlyOrderEvents, hasEvent(PaymentErrorReceivedEvent.class));
+  }
+
+  @Test
+  public void
+  givenMemberWithDirectDebitMandate_WhenCreatingChargeAndTrustlyReturnsSuccess_ThenShouldReturnTransactionId()
+    throws Exception {
+    commandGateway.sendAndWait(new CreateMemberCommand(TOLVANSSON_MEMBER_ID));
+    commandGateway.sendAndWait(
+      new UpdateTrustlyAccountCommand(
+        TOLVANSSON_MEMBER_ID,
+        HEDVIG_ORDER_ID,
+        TRUSTLY_ACCOUNT_ID,
+        TOLVANSSON_STREET,
+        TRUSTLY_ACCOUNT_BANK,
+        TOLVANSSON_CITY,
+        TRUSTLY_ACCOUNT_CLEARING_HOUSE,
+        TRUSTLY_ACCOUNT_DESCRIPTOR,
+        TRUSTLY_ACCOUNT_DIRECTDEBIT_TRUE,
+        TRUSTLY_ACCOUNT_LAST_DIGITS,
+        TOLVAN_FIRST_NAME + " " + TOLVANSSON_LAST_NAME,
+        TOLVANSSON_SSN,
+        TOLVANSSON_ZIP));
+
+    mockTrustlyApiResponse(TrustlyApiResponseResult.SHOULD_SUCCEED);
+    given(uuidGenerator.generateRandom()).willReturn(HEDVIG_ORDER_ID);
+
+    val chargeRequest = new ChargeRequest(TRANSACTION_AMOUNT);
+
+    mockMvc
+      .perform(
+        post(String.format("/v2/_/members/%s/charge", TOLVANSSON_MEMBER_ID))
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(chargeRequest)))
+      .andExpect(status().isAccepted()).andExpect(content().string('"' + HEDVIG_ORDER_ID.toString() + '"'));
+
+    val memberEvents =
+      eventStore.readEvents(TOLVANSSON_MEMBER_ID).asStream().collect(Collectors.toList());
+    assertThat(memberEvents, hasEvent(ChargeCreatedEvent.class));
+
+    val trustlyOrderEvents =
+      eventStore.readEvents(HEDVIG_ORDER_ID.toString()).asStream().collect(Collectors.toList());
+    assertThat(trustlyOrderEvents, hasEvent(PaymentResponseReceivedEvent.class));
   }
 
   private void mockTrustlyApiResponse(TrustlyApiResponseResult result) {
