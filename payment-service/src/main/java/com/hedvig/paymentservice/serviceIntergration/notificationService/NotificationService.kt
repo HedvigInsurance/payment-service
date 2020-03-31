108973ecfdb2cad52b9bab4dff9accbb97cecaf6
Johan Tjelldén <johan@hedvig.com>
2020-03-31 20:33:56 +0200

    Replaced segement for customerio updates

---
+++ payment-service/src/main/java/com/hedvig/paymentservice/serviceIntergration/notificationService/NotificationService.kt
@@ -0,0 +1,11 @@
+package com.hedvig.paymentservice.serviceIntergration.notificationService
+
+import com.fasterxml.jackson.databind.JsonNode
+
+interface NotificationService {
+
+  fun updateCustomer(memberId:String, data:Map<String, Any?>)
+
+  fun updateCustomer(memberId:String, data: JsonNode)
+
+}
---
+++ payment-service/src/main/java/com/hedvig/paymentservice/serviceIntergration/notificationService/NotificationServiceClient.kt
@@ -0,0 +1,15 @@
+package com.hedvig.paymentservice.serviceIntergration.notificationService
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@FeignClient("NotificationServiceClient", url = "\${hedvig.notification-service.url:notification-service}")
+interface NotificationServiceClient {
+
+  @PostMapping("/_/customerio/{memberId}")
+  fun updateCustomer(@PathVariable memberId:String, @RequestBody data:JsonNode)
+
+}
