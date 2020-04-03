2d44ba4124a36e4c0d6fd4de949004c466376217
meletios <meletios@hedvig.com>
2020-04-03 14:16:28 +0200

    add service for submiting adyen redirection

--- payment-service/src/main/java/com/hedvig/paymentservice/services/adyen/AdyenService.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/services/adyen/AdyenService.kt
@@ -3,6 +3,8 @@ package com.hedvig.paymentservice.services.adyen
 import com.adyen.model.checkout.PaymentsDetailsRequest
 import com.hedvig.paymentservice.graphQl.types.ActivePaymentMethodsResponse
 import com.hedvig.paymentservice.graphQl.types.AvailablePaymentMethodsResponse
+import com.hedvig.paymentservice.graphQl.types.SubmitAdyenRedirectionRequest
+import com.hedvig.paymentservice.graphQl.types.SubmitAdyenRedirectionResponse
 import com.hedvig.paymentservice.graphQl.types.TokenizationRequest
 import com.hedvig.paymentservice.services.adyen.dtos.AdyenPaymentsResponse
 import com.hedvig.paymentservice.services.payments.dto.ChargeMemberRequest
@@ -13,5 +15,6 @@ interface AdyenService {
   fun chargeMemberWithToken(req: ChargeMemberRequest): Any
   fun getActivePaymentMethods(memberId: String): ActivePaymentMethodsResponse?
   fun submitAdditionalPaymentDetails(req: PaymentsDetailsRequest, memberId: String): AdyenPaymentsResponse
+  fun submitAdyenRedirection(req: SubmitAdyenRedirectionRequest, memberId: String): SubmitAdyenRedirectionResponse
   fun fetchAdyenPublicKey(): String
 }
