cdc6d0dc561a01c006407db01b95cf0082dace9d
meletios <meletios@hedvig.com>
2020-04-03 14:17:40 +0200

    store in the db

--- payment-service/src/main/java/com/hedvig/paymentservice/query/adyenTokenRegistration/entities/AdyenTokenRegistration.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/adyenTokenRegistration/entities/AdyenTokenRegistration.java
@@ -19,6 +19,7 @@ public class AdyenTokenRegistration {
   @Enumerated(EnumType.STRING)
   private AdyenTokenRegistrationStatus tokenStatus;
   private String recurringDetailReference;
+  private String paymentDataFromAction;
   @CreationTimestamp
   Instant createdAt;
   @UpdateTimestamp
@@ -43,8 +44,16 @@ public class AdyenTokenRegistration {
     return this.recurringDetailReference;
   }
 
+  public String getPaymentDataFromAction() {
+    return this.paymentDataFromAction;
+  }
+
   public Instant getCreatedAt() {
     return this.createdAt;
+  }
+
+  public Instant getUpdatedAt() {
+    return this.updatedAt;
   }
 
   public void setAdyenTokenRegistrationId(UUID adyenTokenRegistrationId) {
@@ -63,15 +72,19 @@ public class AdyenTokenRegistration {
     this.recurringDetailReference = recurringDetailReference;
   }
 
+  public void setPaymentDataFromAction(String paymentDataFromAction) {
+    this.paymentDataFromAction = paymentDataFromAction;
+  }
+
   public boolean equals(final Object o) {
     if (o == this) return true;
     if (!(o instanceof AdyenTokenRegistration))
       return false;
     final AdyenTokenRegistration other = (AdyenTokenRegistration) o;
     if (!other.canEqual((Object) this)) return false;
-    final Object this$tokenRegistrationId = this.getAdyenTokenRegistrationId();
-    final Object other$tokenRegistrationId = other.getAdyenTokenRegistrationId();
-    if (this$tokenRegistrationId == null ? other$tokenRegistrationId != null : !this$tokenRegistrationId.equals(other$tokenRegistrationId))
+    final Object this$adyenTokenRegistrationId = this.getAdyenTokenRegistrationId();
+    final Object other$adyenTokenRegistrationId = other.getAdyenTokenRegistrationId();
+    if (this$adyenTokenRegistrationId == null ? other$adyenTokenRegistrationId != null : !this$adyenTokenRegistrationId.equals(other$adyenTokenRegistrationId))
       return false;
     final Object this$memberId = this.getMemberId();
     final Object other$memberId = other.getMemberId();
@@ -84,9 +97,16 @@ public class AdyenTokenRegistration {
     final Object other$recurringDetailReference = other.getRecurringDetailReference();
     if (this$recurringDetailReference == null ? other$recurringDetailReference != null : !this$recurringDetailReference.equals(other$recurringDetailReference))
       return false;
+    final Object this$paymentDataFromAction = this.getPaymentDataFromAction();
+    final Object other$paymentDataFromAction = other.getPaymentDataFromAction();
+    if (this$paymentDataFromAction == null ? other$paymentDataFromAction != null : !this$paymentDataFromAction.equals(other$paymentDataFromAction))
+      return false;
     final Object this$createdAt = this.getCreatedAt();
     final Object other$createdAt = other.getCreatedAt();
     if (this$createdAt == null ? other$createdAt != null : !this$createdAt.equals(other$createdAt)) return false;
+    final Object this$updatedAt = this.getUpdatedAt();
+    final Object other$updatedAt = other.getUpdatedAt();
+    if (this$updatedAt == null ? other$updatedAt != null : !this$updatedAt.equals(other$updatedAt)) return false;
     return true;
   }
 
@@ -97,20 +117,24 @@ public class AdyenTokenRegistration {
   public int hashCode() {
     final int PRIME = 59;
     int result = 1;
-    final Object $tokenRegistrationId = this.getAdyenTokenRegistrationId();
-    result = result * PRIME + ($tokenRegistrationId == null ? 43 : $tokenRegistrationId.hashCode());
+    final Object $adyenTokenRegistrationId = this.getAdyenTokenRegistrationId();
+    result = result * PRIME + ($adyenTokenRegistrationId == null ? 43 : $adyenTokenRegistrationId.hashCode());
     final Object $memberId = this.getMemberId();
     result = result * PRIME + ($memberId == null ? 43 : $memberId.hashCode());
     final Object $tokenStatus = this.getTokenStatus();
     result = result * PRIME + ($tokenStatus == null ? 43 : $tokenStatus.hashCode());
     final Object $recurringDetailReference = this.getRecurringDetailReference();
     result = result * PRIME + ($recurringDetailReference == null ? 43 : $recurringDetailReference.hashCode());
+    final Object $paymentDataFromAction = this.getPaymentDataFromAction();
+    result = result * PRIME + ($paymentDataFromAction == null ? 43 : $paymentDataFromAction.hashCode());
     final Object $createdAt = this.getCreatedAt();
     result = result * PRIME + ($createdAt == null ? 43 : $createdAt.hashCode());
+    final Object $updatedAt = this.getUpdatedAt();
+    result = result * PRIME + ($updatedAt == null ? 43 : $updatedAt.hashCode());
     return result;
   }
 
   public String toString() {
-    return "AdyenTokenRegistration(tokenRegistrationId=" + this.getAdyenTokenRegistrationId() + ", memberId=" + this.getMemberId() + ", tokenStatus=" + this.getTokenStatus() + ", recurringDetailReference=" + this.getRecurringDetailReference() + ", createdAt=" + this.getCreatedAt() + ")";
+    return "AdyenTokenRegistration(adyenTokenRegistrationId=" + this.getAdyenTokenRegistrationId() + ", memberId=" + this.getMemberId() + ", tokenStatus=" + this.getTokenStatus() + ", recurringDetailReference=" + this.getRecurringDetailReference() + ", paymentDataFromAction=" + this.getPaymentDataFromAction() + ", createdAt=" + this.getCreatedAt() + ", updatedAt=" + this.getUpdatedAt() + ")";
   }
 }
