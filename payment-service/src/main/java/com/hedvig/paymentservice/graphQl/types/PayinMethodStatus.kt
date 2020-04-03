efde1e98f80fe28535ba61e875740be7704410f8
meletios <meletios@hedvig.com>
2020-04-03 07:32:32 +0200

    add  a new helper function to enum

--- payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/PayinMethodStatus.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/PayinMethodStatus.kt
@@ -1,5 +1,7 @@
 package com.hedvig.paymentservice.graphQl.types
 
+import com.hedvig.paymentservice.domain.payments.enums.AdyenAccountStatus
+
 enum class PayinMethodStatus {
   ACTIVE, PENDING, NEEDS_SETUP;
 
@@ -10,6 +12,14 @@ enum class PayinMethodStatus {
         DirectDebitStatus.ACTIVE -> ACTIVE
         DirectDebitStatus.NEEDS_SETUP -> NEEDS_SETUP
         DirectDebitStatus.PENDING -> PENDING
+      }
+    }
+
+    fun fromAdyenAccountStatus(registrationStatus: AdyenAccountStatus): PayinMethodStatus {
+      return when (registrationStatus) {
+        AdyenAccountStatus.AUTHORISED -> ACTIVE
+        AdyenAccountStatus.PENDING -> PENDING
+        AdyenAccountStatus.CANCELLED -> NEEDS_SETUP
       }
     }
   }
