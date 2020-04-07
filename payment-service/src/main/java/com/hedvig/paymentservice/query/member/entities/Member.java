61b8e53823ea9e2bd16e27be57f6e317d08fc421
meletios <meletios@hedvig.com>
2020-04-07 09:24:25 +0200

    check DD for Adyen

--- payment-service/src/main/java/com/hedvig/paymentservice/query/member/entities/Member.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/member/entities/Member.java
@@ -55,7 +55,8 @@ public class Member {
   }
 
   public Boolean isDirectDebitMandateActive() {
-    return (directDebitStatus != null && directDebitStatus.equals(DirectDebitStatus.CONNECTED)) || (adyenRecurringDetailReference != null && payinMethodStatus.equals(PayinMethodStatus.ACTIVE));
+    return (trustlyAccountNumber != null && directDebitStatus != null && directDebitStatus.equals(DirectDebitStatus.CONNECTED))
+      || (adyenRecurringDetailReference != null && payinMethodStatus.equals(PayinMethodStatus.ACTIVE));
   }
 
   public String getId() {
