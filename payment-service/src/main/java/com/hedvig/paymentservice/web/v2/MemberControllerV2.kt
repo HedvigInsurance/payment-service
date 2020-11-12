9f9ec266ab80047e4da1a1b5a023c0e3f0ab2d7f
meletios <meletios@hedvig.com>
2020-11-12 14:59:30 +0100

    create new charging end point for account-service

--- payment-service/src/main/java/com/hedvig/paymentservice/web/v2/MemberControllerV2.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/web/v2/MemberControllerV2.kt
@@ -7,6 +7,7 @@ import com.hedvig.paymentservice.serviceIntergration.memberService.MemberService
 import com.hedvig.paymentservice.serviceIntergration.memberService.dto.SanctionStatus
 import com.hedvig.paymentservice.services.payments.PaymentService
 import com.hedvig.paymentservice.services.payments.dto.ChargeMemberRequest
+import com.hedvig.paymentservice.services.payments.dto.ChargeMemberResult
 import com.hedvig.paymentservice.services.payments.dto.ChargeMemberResultType
 import com.hedvig.paymentservice.services.payments.dto.PayoutMemberRequestDTO
 import com.hedvig.paymentservice.web.dtos.ChargeRequest
@@ -22,89 +23,98 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
-import java.util.*
+import java.util.UUID
 
 @RestController
 @RequestMapping(path = ["/v2/_/members/"])
 class MemberControllerV2(
-  private val paymentService: PaymentService,
-  private val memberService: MemberService,
-  private val meerkat: Meerkat
+    private val paymentService: PaymentService,
+    private val memberService: MemberService,
+    private val meerkat: Meerkat
 ) {
 
-  @PostMapping("{memberId}/charge")
-  fun chargeMember(@PathVariable memberId: String, @RequestBody request: ChargeRequest): ResponseEntity<UUID> {
-    val result = paymentService.chargeMember(ChargeMemberRequest.fromChargeRequest(memberId, request))
+    @PostMapping("{memberId}/charge")
+    fun chargeMember(@PathVariable memberId: String, @RequestBody request: ChargeRequest): ResponseEntity<UUID> {
+        val result = paymentService.chargeMember(ChargeMemberRequest.fromChargeRequest(memberId, request))
 
-    return if (result.type != ChargeMemberResultType.SUCCESS) {
-      ResponseEntity.status(HttpStatus.FORBIDDEN)
-        .body(result.transactionId)
-    } else ResponseEntity.accepted().body(result.transactionId)
-  }
-
-  @PostMapping(path = ["{memberId}/payout"])
-  fun payoutMember(
-    @PathVariable memberId: String,
-    @RequestParam(
-      name = "category",
-      required = false,
-      defaultValue = "CLAIM"
-    ) category: TransactionCategory,
-    @RequestParam(
-      name = "referenceId",
-      required = false
-    ) referenceId: String?,
-    @RequestParam(name = "note", required = false) note: String?,
-    @RequestParam(name = "handler", required = false) handler: String?,
-    @RequestBody request: PayoutRequestDTO
-  ): ResponseEntity<UUID> {
-    if (category != TransactionCategory.CLAIM &&
-      request.amount.number.numberValueExact(BigDecimal::class.java) > BigDecimal.valueOf(10000)
-    ) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .build()
+        return if (result.type != ChargeMemberResultType.SUCCESS) {
+            ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(result.transactionId)
+        } else ResponseEntity.accepted().body(result.transactionId)
     }
 
-    val optionalMember = memberService.getMember(memberId)
-    if (!optionalMember.isPresent) {
-      return ResponseEntity.notFound().build()
+    @PostMapping("{memberId}/autocharge")
+    fun chargeMemberAutomatically(
+        @PathVariable memberId: String,
+        @RequestBody request: ChargeRequest
+    ): ResponseEntity<ChargeMemberResult> {
+        val result = paymentService.chargeMember(ChargeMemberRequest.fromChargeRequest(memberId, request))
+        return ResponseEntity.ok(result)
     }
 
-    val member = optionalMember.get()
-    val memberStatus = meerkat.getMemberSanctionStatus(member.firstName + ' ' + member.lastName)
-    if (memberStatus == SanctionStatus.FullHit) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+    @PostMapping(path = ["{memberId}/payout"])
+    fun payoutMember(
+        @PathVariable memberId: String,
+        @RequestParam(
+            name = "category",
+            required = false,
+            defaultValue = "CLAIM"
+        ) category: TransactionCategory,
+        @RequestParam(
+            name = "referenceId",
+            required = false
+        ) referenceId: String?,
+        @RequestParam(name = "note", required = false) note: String?,
+        @RequestParam(name = "handler", required = false) handler: String?,
+        @RequestBody request: PayoutRequestDTO
+    ): ResponseEntity<UUID> {
+        if (category != TransactionCategory.CLAIM &&
+            request.amount.number.numberValueExact(BigDecimal::class.java) > BigDecimal.valueOf(10000)
+        ) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .build()
+        }
+
+        val optionalMember = memberService.getMember(memberId)
+        if (!optionalMember.isPresent) {
+            return ResponseEntity.notFound().build()
+        }
+
+        val member = optionalMember.get()
+        val memberStatus = meerkat.getMemberSanctionStatus(member.firstName + ' ' + member.lastName)
+        if (memberStatus == SanctionStatus.FullHit) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
+        if (!request.sanctionBypassed &&
+            (memberStatus == SanctionStatus.Undetermined || memberStatus == SanctionStatus.PartialHit)
+        ) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
+        val payoutMemberRequest = PayoutMemberRequestDTO(
+            request.amount,
+            category,
+            referenceId,
+            note,
+            handler
+        )
+        val result = paymentService.payoutMember(memberId, member, payoutMemberRequest)
+
+        return result.map { uuid -> ResponseEntity.accepted().body(uuid) }.orElseGet {
+            ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build()
+        }
     }
 
-    if (!request.sanctionBypassed &&
-      (memberStatus == SanctionStatus.Undetermined || memberStatus == SanctionStatus.PartialHit)
-    ) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+    @GetMapping("payinProvider/{payinProvider}")
+    fun getMembersConnectedToProvider(
+        @PathVariable payinProvider: PayinProvider
+    ): ResponseEntity<List<String>> =
+        ResponseEntity.ok(memberService.getMembersByPayinProvider(payinProvider))
+
+    companion object {
+        val logger = LoggerFactory.getLogger(this::class.java)!!
     }
-
-    val payoutMemberRequest = PayoutMemberRequestDTO(
-      request.amount,
-      category,
-      referenceId,
-      note,
-      handler
-    )
-    val result = paymentService.payoutMember(memberId, member, payoutMemberRequest)
-
-    return result.map { uuid -> ResponseEntity.accepted().body(uuid) }.orElseGet {
-      ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build()
-    }
-  }
-
-  @GetMapping("payinProvider/{payinProvider}")
-  fun getMembersConnectedToProvider(
-    @PathVariable payinProvider: PayinProvider
-  ): ResponseEntity<List<String>> =
-    ResponseEntity.ok(memberService.getMembersByPayinProvider(payinProvider))
-
-  companion object {
-    val logger = LoggerFactory.getLogger(this::class.java)!!
-  }
 
 }
 
