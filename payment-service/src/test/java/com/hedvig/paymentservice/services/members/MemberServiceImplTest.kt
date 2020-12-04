4cb084235caa87f54332471fa98d495555a8da42
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2020-12-04 09:35:52 +0100

    does member have their payments connected

--- payment-service/src/test/java/com/hedvig/paymentservice/services/members/MemberServiceImplTest.kt
+++ payment-service/src/test/java/com/hedvig/paymentservice/services/members/MemberServiceImplTest.kt
@@ -2,150 +2,181 @@ package com.hedvig.paymentservice.services.members
 
 import com.hedvig.paymentservice.domain.payments.DirectDebitStatus
 import com.hedvig.paymentservice.domain.payments.enums.PayinProvider
+import com.hedvig.paymentservice.query.member.entities.Member
 import com.hedvig.paymentservice.query.member.entities.MemberRepository
 import com.hedvig.paymentservice.serviceIntergration.memberService.MemberServiceClient
 import com.hedvig.paymentservice.serviceIntergration.memberService.MemberServiceImpl
+import com.hedvig.paymentservice.serviceIntergration.productPricing.dto.Market
 import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
-import com.hedvig.paymentservice.query.member.entities.Member
-import io.mockk.every
-import org.assertj.core.api.Assertions.assertThat
+import java.util.Optional
 
 @RunWith(MockitoJUnitRunner::class)
 class MemberServiceImplTest {
 
-  @Mock
-  lateinit var memberServiceClient: MemberServiceClient
-  @Mock
-  lateinit var memberRepository: MemberRepository
+    @Mock
+    lateinit var memberServiceClient: MemberServiceClient
 
-  lateinit var classUnderTest: MemberServiceImpl
+    @Mock
+    lateinit var memberRepository: MemberRepository
 
-  @Before
-  fun before() {
-    classUnderTest = MemberServiceImpl(
-      memberServiceClient,
-      memberRepository
-    )
-  }
+    lateinit var classUnderTest: MemberServiceImpl
 
-  @Test
-  fun `get one member with trustly connected when one member with trustly is provided`() {
-    whenever(memberRepository.findAll()).thenReturn(listOf(
-      buildMemberEntity(
-        trustlyAccountNumber = "123",
-        directDebitStatus = DirectDebitStatus.CONNECTED
-      )
-    ))
+    @Before
+    fun before() {
+        classUnderTest = MemberServiceImpl(
+            memberServiceClient,
+            memberRepository
+        )
+    }
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
+    @Test
+    fun `get one member with trustly connected when one member with trustly is provided`() {
+        whenever(memberRepository.findAll()).thenReturn(listOf(
+            buildMemberEntity(
+                trustlyAccountNumber = "123",
+                directDebitStatus = DirectDebitStatus.CONNECTED
+            )
+        ))
 
-    assertThat(result).contains("321")
-  }
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
 
-  @Test
-  fun `get two member with trustly connected when two member with trustly is provided`() {
-    whenever(memberRepository.findAll()).thenReturn(listOf(
-      buildMemberEntity(
-        trustlyAccountNumber = "123",
-        directDebitStatus = DirectDebitStatus.CONNECTED
-      ),
-      buildMemberEntity(
-        id = "1234",
-        trustlyAccountNumber = "123",
-        directDebitStatus = DirectDebitStatus.CONNECTED
-      )
-    ))
+        assertThat(result).contains("321")
+    }
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
+    @Test
+    fun `get two member with trustly connected when two member with trustly is provided`() {
+        whenever(memberRepository.findAll()).thenReturn(listOf(
+            buildMemberEntity(
+                trustlyAccountNumber = "123",
+                directDebitStatus = DirectDebitStatus.CONNECTED
+            ),
+            buildMemberEntity(
+                id = "1234",
+                trustlyAccountNumber = "123",
+                directDebitStatus = DirectDebitStatus.CONNECTED
+            )
+        ))
 
-    assertThat(result).contains("321", "1234")
-  }
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
 
-  @Test
-  fun `get one member with adyen connected when one member with adyen is provided`() {
-    whenever(memberRepository.findAll()).thenReturn(listOf(
-      buildMemberEntity(
-        adyenRecurringDetailReference = "ref"
-      )
-    ))
+        assertThat(result).contains("321", "1234")
+    }
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.ADYEN)
+    @Test
+    fun `get one member with adyen connected when one member with adyen is provided`() {
+        whenever(memberRepository.findAll()).thenReturn(listOf(
+            buildMemberEntity(
+                adyenRecurringDetailReference = "ref"
+            )
+        ))
 
-    assertThat(result).contains("321")
-  }
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.ADYEN)
+
+        assertThat(result).contains("321")
+    }
 
 
-  @Test
-  fun `get one member with trustly connected when one member with trustly and one with adyen is provided`() {
-    whenever(memberRepository.findAll()).thenReturn(listOf(
-      buildMemberEntity(
-        trustlyAccountNumber = "123",
-        directDebitStatus = DirectDebitStatus.CONNECTED
-      ),
-      buildMemberEntity(
-        id = "1234",
-        adyenRecurringDetailReference = "ref"
-      )
-    ))
+    @Test
+    fun `get one member with trustly connected when one member with trustly and one with adyen is provided`() {
+        whenever(memberRepository.findAll()).thenReturn(listOf(
+            buildMemberEntity(
+                trustlyAccountNumber = "123",
+                directDebitStatus = DirectDebitStatus.CONNECTED
+            ),
+            buildMemberEntity(
+                id = "1234",
+                adyenRecurringDetailReference = "ref"
+            )
+        ))
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
 
-    assertThat(result).contains("321")
-  }
+        assertThat(result).contains("321")
+    }
 
-  @Test
-  fun `get one member with adyen connected when one member with trustly and one with adyen is provided`() {
-    whenever(memberRepository.findAll()).thenReturn(listOf(
-      buildMemberEntity(
-        trustlyAccountNumber = "123",
-        directDebitStatus = DirectDebitStatus.CONNECTED
-      ),
-      buildMemberEntity(
-        id = "1234",
-        adyenRecurringDetailReference = "ref"
-      )
-    ))
+    @Test
+    fun `get one member with adyen connected when one member with trustly and one with adyen is provided`() {
+        whenever(memberRepository.findAll()).thenReturn(listOf(
+            buildMemberEntity(
+                trustlyAccountNumber = "123",
+                directDebitStatus = DirectDebitStatus.CONNECTED
+            ),
+            buildMemberEntity(
+                id = "1234",
+                adyenRecurringDetailReference = "ref"
+            )
+        ))
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.ADYEN)
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.ADYEN)
 
-    assertThat(result).contains("1234")
-  }
+        assertThat(result).contains("1234")
+    }
 
-  @Test
-  fun `if pay in provider is Trustly only return members who have direct debit connected`() {
-    val member1 = buildMemberEntity(
-      trustlyAccountNumber = "322",
-      directDebitStatus = DirectDebitStatus.DISCONNECTED
-    )
-    val member2 = buildMemberEntity(
-      trustlyAccountNumber = "222",
-      directDebitStatus = DirectDebitStatus.CONNECTED
-    )
+    @Test
+    fun `if pay in provider is Trustly only return members who have direct debit connected`() {
+        val member1 = buildMemberEntity(
+            trustlyAccountNumber = "322",
+            directDebitStatus = DirectDebitStatus.DISCONNECTED
+        )
+        val member2 = buildMemberEntity(
+            trustlyAccountNumber = "222",
+            directDebitStatus = DirectDebitStatus.CONNECTED
+        )
 
-    whenever(memberRepository.findAll()).thenReturn(listOf(member1, member2))
+        whenever(memberRepository.findAll()).thenReturn(listOf(member1, member2))
 
-    val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
+        val result = classUnderTest.getMembersByPayinProvider(PayinProvider.TRUSTLY)
 
-    assertThat(result.size).isEqualTo(1)
-  }
+        assertThat(result.size).isEqualTo(1)
+    }
 
-  private fun buildMemberEntity(
-    id: String = "321",
-    trustlyAccountNumber: String? = null,
-    adyenRecurringDetailReference: String? = null,
-    directDebitStatus: DirectDebitStatus? = null
-  ): Member {
-    val member = Member()
-    member.id = id
-    member.trustlyAccountNumber = trustlyAccountNumber
-    member.adyenRecurringDetailReference = adyenRecurringDetailReference
-    member.directDebitStatus = directDebitStatus
+    @Test
+    fun `if market is sweden return true if direct debit status is connected`() {
+        val member = buildMemberEntity(
+            trustlyAccountNumber = "322",
+            directDebitStatus = DirectDebitStatus.CONNECTED
+        )
 
-    return member
-  }
+        whenever(memberRepository.findById("322")).thenReturn(Optional.of(member))
+
+        val result = classUnderTest.hasMemberConnectedPaymentForMarket("322", Market.SWEDEN)
+
+        assertThat(result).isTrue()
+    }
+
+
+    @Test
+    fun `if market is sweden return false if direct debit status is not connected`() {
+        val member = buildMemberEntity(
+            trustlyAccountNumber = "5463",
+            directDebitStatus = DirectDebitStatus.PENDING
+        )
+
+        whenever(memberRepository.findById("222")).thenReturn(Optional.of(member))
+
+        val result = classUnderTest.hasMemberConnectedPaymentForMarket("222", Market.SWEDEN)
+
+        assertThat(result).isFalse()
+    }
+
+    private fun buildMemberEntity(
+        id: String = "321",
+        trustlyAccountNumber: String? = null,
+        adyenRecurringDetailReference: String? = null,
+        directDebitStatus: DirectDebitStatus? = null
+    ): Member {
+        val member = Member()
+        member.id = id
+        member.trustlyAccountNumber = trustlyAccountNumber
+        member.adyenRecurringDetailReference = adyenRecurringDetailReference
+        member.directDebitStatus = directDebitStatus
+
+        return member
+    }
 }
