a05c3ba03fee84e40c9e4bf101ad8e73a6b2af8e
meletios <meletios@hedvig.com>
2020-12-18 09:42:32 +0200

    splitting direct debit to its own event listener

---
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/member/DirectDebitEventListener.kt
@@ -0,0 +1,85 @@
+package com.hedvig.paymentservice.query.member
+
+import com.hedvig.paymentservice.domain.payments.DirectDebitStatus
+import com.hedvig.paymentservice.domain.payments.events.DirectDebitConnectedEvent
+import com.hedvig.paymentservice.domain.payments.events.DirectDebitDisconnectedEvent
+import com.hedvig.paymentservice.domain.payments.events.DirectDebitPendingConnectionEvent
+import com.hedvig.paymentservice.domain.payments.events.TrustlyAccountCreatedEvent
+import com.hedvig.paymentservice.domain.payments.events.TrustlyAccountUpdatedEvent
+import com.hedvig.paymentservice.query.member.entities.MemberRepository
+import lombok.extern.slf4j.Slf4j
+import org.axonframework.config.ProcessingGroup
+import org.axonframework.eventhandling.EventHandler
+import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+
+@Component
+@Slf4j
+@Order(1)
+@ProcessingGroup("DirectDebitProcessorGroup")
+class DirectDebitEventListener(
+    val memberRepository: MemberRepository
+) {
+    @EventHandler
+    fun on(e: TrustlyAccountCreatedEvent) {
+        val member = memberRepository.findById(e.memberId)
+        if (!member.isPresent) {
+            logger.error("Could not find member")
+            return
+        }
+        val m = member.get()
+        m.trustlyAccountNumber = e.trustlyAccountId
+        m.bank = e.bank
+        m.descriptor = e.descriptor
+        memberRepository.save(m)
+    }
+
+    @EventHandler
+    fun on(e: TrustlyAccountUpdatedEvent) {
+        val member = memberRepository.findById(e.memberId)
+        if (!member.isPresent) {
+            logger.error("Could not find member")
+            return
+        }
+        val m = member.get()
+        m.trustlyAccountNumber = e.trustlyAccountId
+        m.bank = e.bank
+        m.descriptor = e.descriptor
+        memberRepository.save(m)
+    }
+
+    @EventHandler
+    fun on(e: DirectDebitConnectedEvent) {
+        updateDirectDebitStatus(DirectDebitStatus.CONNECTED, e.memberId, e.trustlyAccountId)
+    }
+
+    @EventHandler
+    fun on(e: DirectDebitPendingConnectionEvent) {
+        updateDirectDebitStatus(DirectDebitStatus.PENDING, e.memberId, e.trustlyAccountId)
+    }
+
+    @EventHandler
+    fun on(e: DirectDebitDisconnectedEvent) {
+        updateDirectDebitStatus(DirectDebitStatus.DISCONNECTED, e.memberId, e.trustlyAccountId)
+    }
+
+    private fun updateDirectDebitStatus(status: DirectDebitStatus, memberId: String, trustlyAccountId: String) {
+        val optionalMember = memberRepository.findById(memberId)
+        if (!optionalMember.isPresent) {
+            logger.error(
+                "Cannot update direct debit status! Member {} cannot be found. TrustlyAccountId: {}",
+                memberId,
+                trustlyAccountId
+            )
+            return
+        }
+        val m = optionalMember.get()
+        m.directDebitStatus = status
+        memberRepository.save(m)
+    }
+
+    companion object{
+        val logger = LoggerFactory.getLogger(this::class.java)!!
+    }
+}
--- payment-service/src/main/java/com/hedvig/paymentservice/query/member/MemberEventListener.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/member/MemberEventListener.java
@@ -1,6 +1,5 @@
 package com.hedvig.paymentservice.query.member;
 
-import com.hedvig.paymentservice.domain.payments.DirectDebitStatus;
 import com.hedvig.paymentservice.domain.payments.TransactionStatus;
 import com.hedvig.paymentservice.domain.payments.TransactionType;
 import com.hedvig.paymentservice.domain.payments.events.AdyenAccountCreatedEvent;
@@ -8,15 +7,10 @@ import com.hedvig.paymentservice.domain.payments.events.AdyenAccountUpdatedEvent
 import com.hedvig.paymentservice.domain.payments.events.ChargeCompletedEvent;
 import com.hedvig.paymentservice.domain.payments.events.ChargeCreatedEvent;
 import com.hedvig.paymentservice.domain.payments.events.ChargeFailedEvent;
-import com.hedvig.paymentservice.domain.payments.events.DirectDebitConnectedEvent;
-import com.hedvig.paymentservice.domain.payments.events.DirectDebitDisconnectedEvent;
-import com.hedvig.paymentservice.domain.payments.events.DirectDebitPendingConnectionEvent;
 import com.hedvig.paymentservice.domain.payments.events.MemberCreatedEvent;
 import com.hedvig.paymentservice.domain.payments.events.PayoutCompletedEvent;
 import com.hedvig.paymentservice.domain.payments.events.PayoutCreatedEvent;
 import com.hedvig.paymentservice.domain.payments.events.PayoutFailedEvent;
-import com.hedvig.paymentservice.domain.payments.events.TrustlyAccountCreatedEvent;
-import com.hedvig.paymentservice.domain.payments.events.TrustlyAccountUpdatedEvent;
 import com.hedvig.paymentservice.graphQl.types.PayinMethodStatus;
 import com.hedvig.paymentservice.query.member.entities.Member;
 import com.hedvig.paymentservice.query.member.entities.MemberRepository;
@@ -38,215 +32,151 @@ import java.util.Optional;
 @ProcessingGroup("MemberEventsProcessorGroup")
 public class MemberEventListener {
 
-  private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-  public MemberEventListener(MemberRepository memberRepository) {
-    this.memberRepository = memberRepository;
-  }
-
-  @EventHandler
-  public void on(MemberCreatedEvent e) {
-    val member = new Member();
-    member.setId(e.getMemberId());
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(ChargeCreatedEvent e) {
-    val member =
-      memberRepository
-        .findById(e.getMemberId())
-        .orElseThrow(() -> new RuntimeException("Could not find member"));
-    val transaction = new Transaction();
-    transaction.setId(e.getTransactionId());
-    transaction.setAmount(e.getAmount().getNumber().numberValueExact(BigDecimal.class));
-    transaction.setCurrency(e.getAmount().getCurrency().getCurrencyCode());
-    transaction.setTimestamp(e.getTimestamp());
-    transaction.setTransactionType(TransactionType.CHARGE);
-    transaction.setTransactionStatus(TransactionStatus.INITIATED);
-    transaction.setMember(member);
-
-    val transactions = member.getTransactions();
-    transactions.put(transaction.getId(), transaction);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(ChargeFailedEvent e) {
-    val member =
-      memberRepository
-        .findById(e.getMemberId())
-        .orElseThrow(() -> new RuntimeException("Could not find member"));
-    val transactions = member.getTransactions();
-    val transaction = transactions.get(e.getTransactionId());
-    transaction.setTransactionStatus(TransactionStatus.FAILED);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(PayoutCreatedEvent e) {
-    val member =
-      memberRepository
-        .findById(e.getMemberId())
-        .orElseThrow(() -> new RuntimeException("Could not find member"));
-    val transaction = new Transaction();
-    transaction.setId(e.getTransactionId());
-    transaction.setAmount(e.getAmount().getNumber().numberValueExact(BigDecimal.class));
-    transaction.setCurrency(e.getAmount().getCurrency().getCurrencyCode());
-    transaction.setTimestamp(e.getTimestamp());
-    transaction.setTransactionType(TransactionType.PAYOUT);
-    transaction.setTransactionStatus(TransactionStatus.INITIATED);
-    transaction.setMember(member);
-
-    val transactions = member.getTransactions();
-    transactions.put(e.getTransactionId(), transaction);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(ChargeCompletedEvent e) {
-    val maybeMember = memberRepository.findById(e.getMemberId());
-    if (maybeMember.isPresent() == false) {
-      log.error("Could not find member");
-      return;
-    }
-    val member = maybeMember.get();
-
-    val transaction = member.getTransactions().get(e.getTransactionId());
-
-    transaction.setTransactionStatus(TransactionStatus.COMPLETED);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(PayoutCompletedEvent e) {
-    val maybeMember = memberRepository.findById(e.getMemberId());
-    if (maybeMember.isPresent() == false) {
-      log.error("Could not find member");
-      return;
+    public MemberEventListener(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    final Member member = maybeMember.get();
-    val transaction = member.getTransaction(e.getTransactionId());
-    transaction.setTransactionStatus(TransactionStatus.COMPLETED);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(PayoutFailedEvent e) {
-    val maybeMember = memberRepository.findById(e.getMemberId());
-    if (maybeMember.isPresent() == false) {
-      log.error("Could not find member");
-      return;
-    }
-    val member = maybeMember.get();
-
-    val transaction = member.getTransaction(e.getTransactionId());
-    transaction.setTransactionStatus(TransactionStatus.FAILED);
-    memberRepository.save(member);
-  }
-
-  @EventHandler
-  public void on(TrustlyAccountCreatedEvent e) {
-    Optional<Member> member = memberRepository.findById(e.getMemberId());
-
-    if (!member.isPresent()) {
-      log.error("Could not find member");
-      return;
+    @EventHandler
+    public void on(MemberCreatedEvent e) {
+        val member = new Member();
+        member.setId(e.getMemberId());
+        memberRepository.save(member);
     }
 
-    Member m = member.get();
+    @EventHandler
+    public void on(ChargeCreatedEvent e) {
+        val member =
+            memberRepository
+                .findById(e.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Could not find member"));
+        val transaction = new Transaction();
+        transaction.setId(e.getTransactionId());
+        transaction.setAmount(e.getAmount().getNumber().numberValueExact(BigDecimal.class));
+        transaction.setCurrency(e.getAmount().getCurrency().getCurrencyCode());
+        transaction.setTimestamp(e.getTimestamp());
+        transaction.setTransactionType(TransactionType.CHARGE);
+        transaction.setTransactionStatus(TransactionStatus.INITIATED);
+        transaction.setMember(member);
 
-    m.setTrustlyAccountNumber(e.getTrustlyAccountId());
-    m.setBank(e.getBank());
-    m.setDescriptor(e.getDescriptor());
-
-    memberRepository.save(m);
-  }
-
-  @EventHandler
-  public void on(TrustlyAccountUpdatedEvent e) {
-    Optional<Member> member = memberRepository.findById(e.getMemberId());
-
-    if (!member.isPresent()) {
-      log.error("Could not find member");
-      return;
+        val transactions = member.getTransactions();
+        transactions.put(transaction.getId(), transaction);
+        memberRepository.save(member);
     }
 
-    Member m = member.get();
-
-    m.setTrustlyAccountNumber(e.getTrustlyAccountId());
-    m.setBank(e.getBank());
-    m.setDescriptor(e.getDescriptor());
-
-    memberRepository.save(m);
-  }
-
-  @EventHandler
-  public void on(AdyenAccountCreatedEvent e) {
-    Optional<Member> member = memberRepository.findById(e.getMemberId());
-
-    if (!member.isPresent()) {
-      log.error("Could not find member");
-      return;
+    @EventHandler
+    public void on(ChargeFailedEvent e) {
+        val member =
+            memberRepository
+                .findById(e.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Could not find member"));
+        val transactions = member.getTransactions();
+        val transaction = transactions.get(e.getTransactionId());
+        transaction.setTransactionStatus(TransactionStatus.FAILED);
+        memberRepository.save(member);
     }
 
-    Member m = member.get();
+    @EventHandler
+    public void on(PayoutCreatedEvent e) {
+        val member =
+            memberRepository
+                .findById(e.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Could not find member"));
+        val transaction = new Transaction();
+        transaction.setId(e.getTransactionId());
+        transaction.setAmount(e.getAmount().getNumber().numberValueExact(BigDecimal.class));
+        transaction.setCurrency(e.getAmount().getCurrency().getCurrencyCode());
+        transaction.setTimestamp(e.getTimestamp());
+        transaction.setTransactionType(TransactionType.PAYOUT);
+        transaction.setTransactionStatus(TransactionStatus.INITIATED);
+        transaction.setMember(member);
 
-    m.setAdyenRecurringDetailReference(e.getRecurringDetailReference());
-    m.setPayinMethodStatus(PayinMethodStatus.Companion.fromAdyenAccountStatus(e.getAccountStatus()));
-
-    memberRepository.save(m);
-  }
-
-  @EventHandler
-  public void on(AdyenAccountUpdatedEvent e) {
-    Optional<Member> member = memberRepository.findById(e.getMemberId());
-
-    if (!member.isPresent()) {
-      log.error("Could not find member");
-      return;
+        val transactions = member.getTransactions();
+        transactions.put(e.getTransactionId(), transaction);
+        memberRepository.save(member);
     }
 
-    Member m = member.get();
+    @EventHandler
+    public void on(ChargeCompletedEvent e) {
+        val maybeMember = memberRepository.findById(e.getMemberId());
+        if (maybeMember.isPresent() == false) {
+            log.error("Could not find member");
+            return;
+        }
+        val member = maybeMember.get();
 
-    m.setAdyenRecurringDetailReference(e.getRecurringDetailReference());
-    m.setPayinMethodStatus(PayinMethodStatus.Companion.fromAdyenAccountStatus(e.getAccountStatus()));
+        val transaction = member.getTransactions().get(e.getTransactionId());
 
-    memberRepository.save(m);
-  }
-
-  @EventHandler
-  public void on(DirectDebitConnectedEvent e) {
-    updateDirectDebitStatus(DirectDebitStatus.CONNECTED, e.getMemberId(), e.getTrustlyAccountId());
-  }
-
-  @EventHandler
-  public void on(DirectDebitPendingConnectionEvent e) {
-    updateDirectDebitStatus(DirectDebitStatus.PENDING, e.getMemberId(), e.getTrustlyAccountId());
-  }
-
-  @EventHandler
-  public void on(DirectDebitDisconnectedEvent e) {
-    updateDirectDebitStatus(DirectDebitStatus.DISCONNECTED, e.getMemberId(), e.getTrustlyAccountId());
-  }
-
-  @ResetHandler
-  public void onReset() {
-    memberRepository.deleteAll();
-  }
-
-  private void updateDirectDebitStatus(DirectDebitStatus status, String memberId, String trustlyAccountId) {
-    Optional<Member> optionalMember = memberRepository.findById(memberId);
-
-    if (!optionalMember.isPresent()) {
-      log.error("Cannot update direct debit status! Member {} cannot be found. TrustlyAccountId: {}", memberId, trustlyAccountId);
-      return;
+        transaction.setTransactionStatus(TransactionStatus.COMPLETED);
+        memberRepository.save(member);
     }
 
-    Member m = optionalMember.get();
+    @EventHandler
+    public void on(PayoutCompletedEvent e) {
+        val maybeMember = memberRepository.findById(e.getMemberId());
+        if (maybeMember.isPresent() == false) {
+            log.error("Could not find member");
+            return;
+        }
 
-    m.setDirectDebitStatus(status);
-    memberRepository.save(m);
-  }
+        final Member member = maybeMember.get();
+        val transaction = member.getTransaction(e.getTransactionId());
+        transaction.setTransactionStatus(TransactionStatus.COMPLETED);
+        memberRepository.save(member);
+    }
+
+    @EventHandler
+    public void on(PayoutFailedEvent e) {
+        val maybeMember = memberRepository.findById(e.getMemberId());
+        if (maybeMember.isPresent() == false) {
+            log.error("Could not find member");
+            return;
+        }
+        val member = maybeMember.get();
+
+        val transaction = member.getTransaction(e.getTransactionId());
+        transaction.setTransactionStatus(TransactionStatus.FAILED);
+        memberRepository.save(member);
+    }
+
+
+    @EventHandler
+    public void on(AdyenAccountCreatedEvent e) {
+        Optional<Member> member = memberRepository.findById(e.getMemberId());
+
+        if (!member.isPresent()) {
+            log.error("Could not find member");
+            return;
+        }
+
+        Member m = member.get();
+
+        m.setAdyenRecurringDetailReference(e.getRecurringDetailReference());
+        m.setPayinMethodStatus(PayinMethodStatus.Companion.fromAdyenAccountStatus(e.getAccountStatus()));
+
+        memberRepository.save(m);
+    }
+
+    @EventHandler
+    public void on(AdyenAccountUpdatedEvent e) {
+        Optional<Member> member = memberRepository.findById(e.getMemberId());
+
+        if (!member.isPresent()) {
+            log.error("Could not find member");
+            return;
+        }
+
+        Member m = member.get();
+
+        m.setAdyenRecurringDetailReference(e.getRecurringDetailReference());
+        m.setPayinMethodStatus(PayinMethodStatus.Companion.fromAdyenAccountStatus(e.getAccountStatus()));
+
+        memberRepository.save(m);
+    }
+
+    @ResetHandler
+    public void onReset() {
+        memberRepository.deleteAll();
+    }
 }
