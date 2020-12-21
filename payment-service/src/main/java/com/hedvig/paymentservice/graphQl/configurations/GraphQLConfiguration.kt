3f97219bdfb51978e1880edfa038c9ca769a191e
meletios <meletios@hedvig.com>
2020-12-21 17:35:00 +0200

    ☠️

--- payment-service/src/main/java/com/hedvig/paymentservice/graphQl/configurations/GraphQLConfiguration.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/graphQl/configurations/GraphQLConfiguration.kt
@@ -21,7 +21,6 @@ import graphql.schema.CoercingSerializeException
 import graphql.schema.GraphQLScalarType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import kotlin.jvm.Throws
 
 @Configuration
 class GraphQLConfiguration(
