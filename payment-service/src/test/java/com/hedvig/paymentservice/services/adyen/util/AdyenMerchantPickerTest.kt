70e6fbcf76702b290b47f2b5501fbde471c5fbd0
meletios <meletios@hedvig.com>
2021-01-28 15:02:53 +0100

    add reference

--- payment-service/src/test/java/com/hedvig/paymentservice/services/adyen/util/AdyenMerchantPickerTest.kt
+++ payment-service/src/test/java/com/hedvig/paymentservice/services/adyen/util/AdyenMerchantPickerTest.kt
@@ -19,7 +19,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringRunner
-import java.util.UUID
+import java.util.Optional
 import javax.money.Monetary
 
 @RunWith(SpringRunner::class)
