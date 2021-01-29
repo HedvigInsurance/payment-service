22a7256395a9525a843dd5673a3dcf2dfebf13a1
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2021-01-29 10:18:48 +0100

    revert change to Member entity

--- payment-service/src/main/java/com/hedvig/paymentservice/query/member/entities/Member.java
+++ payment-service/src/main/java/com/hedvig/paymentservice/query/member/entities/Member.java
@@ -1,7 +1,12 @@
 package com.hedvig.paymentservice.query.member.entities;
 
+import com.hedvig.paymentservice.domain.payments.DirectDebitStatus;
+import com.hedvig.paymentservice.graphQl.types.PayinMethodStatus;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.MapKey;
 import javax.persistence.OneToMany;
@@ -20,7 +25,30 @@ public class Member {
     @MapKey
     Map<UUID, Transaction> transactions = new HashMap<>();
 
+    String trustlyAccountNumber;
+
+    String adyenRecurringDetailReference;
+
+    String adyenMerchantAccount;
+
+    @Enumerated(EnumType.STRING)
+    DirectDebitStatus directDebitStatus;
+
+    @Enumerated(EnumType.STRING)
+    PayinMethodStatus payinMethodStatus;
+
+    String bank;
+    String descriptor;
+
     public Member() {
+    }
+
+    public PayinMethodStatus getPayinMethodStatus() {
+        return payinMethodStatus;
+    }
+
+    public void setPayinMethodStatus(PayinMethodStatus payinMethodStatus) {
+        this.payinMethodStatus = payinMethodStatus;
     }
 
     public Transaction getTransaction(UUID transactionId) {
@@ -35,6 +63,26 @@ public class Member {
         return this.transactions;
     }
 
+    public String getTrustlyAccountNumber() {
+        return this.trustlyAccountNumber;
+    }
+
+    public String getAdyenRecurringDetailReference() {
+        return this.adyenRecurringDetailReference;
+    }
+
+    public DirectDebitStatus getDirectDebitStatus() {
+        return this.directDebitStatus;
+    }
+
+    public String getBank() {
+        return this.bank;
+    }
+
+    public String getDescriptor() {
+        return this.descriptor;
+    }
+
     public void setId(String id) {
         this.id = id;
     }
@@ -43,17 +91,53 @@ public class Member {
         this.transactions = transactions;
     }
 
+    public void setTrustlyAccountNumber(String trustlyAccountNumber) {
+        this.trustlyAccountNumber = trustlyAccountNumber;
+    }
+
+    public void setAdyenRecurringDetailReference(String adyenRecurringDetailReference) {
+        this.adyenRecurringDetailReference = adyenRecurringDetailReference;
+    }
+
+    public void setDirectDebitStatus(DirectDebitStatus directDebitStatus) {
+        this.directDebitStatus = directDebitStatus;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public void setDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+    }
+
+    public String getAdyenMerchantAccount() {
+        return adyenMerchantAccount;
+    }
+
+    public void setAdyenMerchantAccount(String adyenMerchantAccount) {
+        this.adyenMerchantAccount = adyenMerchantAccount;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Member)) return false;
         Member member = (Member) o;
         return Objects.equals(getId(), member.getId()) &&
-            Objects.equals(getTransactions(), member.getTransactions());
+            Objects.equals(getTransactions(), member.getTransactions()) &&
+            Objects.equals(getTrustlyAccountNumber(), member.getTrustlyAccountNumber()) &&
+            Objects.equals(getAdyenRecurringDetailReference(), member.getAdyenRecurringDetailReference()) &&
+            Objects.equals(getAdyenMerchantAccount(), member.getAdyenMerchantAccount()) &&
+            getDirectDebitStatus() == member.getDirectDebitStatus() &&
+            getPayinMethodStatus() == member.getPayinMethodStatus() &&
+            Objects.equals(getBank(), member.getBank()) &&
+            Objects.equals(getDescriptor(), member.getDescriptor());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getTransactions());
+        return Objects.hash(getId(), getTransactions(), getTrustlyAccountNumber(), getAdyenRecurringDetailReference(), getAdyenMerchantAccount(), getDirectDebitStatus(), getPayinMethodStatus(), getBank(), getDescriptor());
     }
 }
