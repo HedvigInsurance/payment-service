8bbd09f52077e30109cce7a2128e0f1da6912401
vonElfvin <elvin.granat@gmail.com>
2021-02-01 13:35:44 +0100

    Codescoute some stuff

--- payment-service/src/main/java/com/hedvig/paymentservice/domain/payments/events/PayoutCreatedEvent.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/domain/payments/events/PayoutCreatedEvent.kt
@@ -9,20 +9,20 @@ import javax.money.MonetaryAmount
 
 @Revision("1.0")
 data class PayoutCreatedEvent(
-  val memberId: String,
-  val transactionId: UUID,
-  val amount: MonetaryAmount,
-  val address: String?,
-  val countryCode: String?,
-  val dateOfBirth: LocalDate?,
-  val firstName: String?,
-  val lastName: String?,
-  val timestamp: Instant,
-  val trustlyAccountId: String?,
-  val category: TransactionCategory,
-  val referenceId: String?,
-  val note: String?,
-  val handler: String?,
-  val adyenShopperReference: String?,
-  val email: String?
+    val memberId: String,
+    val transactionId: UUID,
+    val amount: MonetaryAmount,
+    val address: String?,
+    val countryCode: String?,
+    val dateOfBirth: LocalDate?,
+    val firstName: String?,
+    val lastName: String?,
+    val timestamp: Instant,
+    val trustlyAccountId: String?,
+    val category: TransactionCategory,
+    val referenceId: String?,
+    val note: String?,
+    val handler: String?,
+    val adyenShopperReference: String?,
+    val email: String?
 )
