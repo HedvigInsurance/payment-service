1e6759b477894e0b2fbb2f1440f8fda099243c57
vonElfvin <elvin.granat@gmail.com>
2021-02-02 11:08:29 +0100

    Typo

--- payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/TokenizationResultType.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/TokenizationResultType.kt
@@ -3,13 +3,13 @@ package com.hedvig.paymentservice.graphQl.types
 import com.hedvig.paymentservice.services.adyen.dtos.PaymentResponseResultCode
 
 enum class TokenizationResultType {
-    AUTHORIZED,
+    AUTHORISED,
     PENDING,
     FAILED;
 
     companion object {
         fun from(resultCode: PaymentResponseResultCode) = when (resultCode) {
-            PaymentResponseResultCode.AUTHORISED -> AUTHORIZED
+            PaymentResponseResultCode.AUTHORISED -> AUTHORISED
             PaymentResponseResultCode.PENDING -> PENDING
             PaymentResponseResultCode.FAILED -> FAILED
         }
