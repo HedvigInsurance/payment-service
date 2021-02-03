f3bba04fe35dbc71de7b450fd7b0ff8283b543cf
vonElfvin <elvin.granat@gmail.com>
2021-02-03 21:29:30 +0100

    Rename the tokenization result type of completion from Authorised to Completed for easier understanding

--- payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/TokenizationResultType.kt
+++ payment-service/src/main/java/com/hedvig/paymentservice/graphQl/types/TokenizationResultType.kt
@@ -3,13 +3,13 @@ package com.hedvig.paymentservice.graphQl.types
 import com.hedvig.paymentservice.services.adyen.dtos.PaymentResponseResultCode
 
 enum class TokenizationResultType {
-    AUTHORISED,
+    COMPLETED,
     PENDING,
     FAILED;
 
     companion object {
         fun from(resultCode: PaymentResponseResultCode) = when (resultCode) {
-            PaymentResponseResultCode.AUTHORISED -> AUTHORISED
+            PaymentResponseResultCode.AUTHORISED -> COMPLETED
             PaymentResponseResultCode.PENDING -> PENDING
             PaymentResponseResultCode.FAILED -> FAILED
         }
